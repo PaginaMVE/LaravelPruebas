0dac370c8f7d5e64322951defb38035829a399ed
Kevin <kevinsl@live.com.mx>
2017-11-10 18:38:51 -0600

    Se creo una migracion y la tabla products

---
+++ database/migrations/2017_11_11_001924_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        //Realiza cambios
+        Schema::create('products', function (Blueprint $tabla) {
+            $tabla->increments('id');
+            
+            $tabla->integer('user_id')->unsigned()->index();
+            $tabla->string('title');
+            $tabla->text('description'); //Tiene mas capacidad que string
+            $tabla->decimal('pricing',9,2); //Centavos----El primer argumento es el nombre del campo, el segundo argument es la preciosion (cuantos numeros aceptara en total todo el campo) y el tercero la escala (dos de ellos seran puntos decimales).
+
+
+            $tabla->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+        //Reinvierte los cambios de up
+        Schema::drop('products');
+    }
+}
